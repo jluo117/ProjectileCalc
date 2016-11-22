25a1551874b6add0ba6947aa7788a7a75a053091
James Luo <luojames52@gmail.com>
2016-11-21 21:46:51 -0800

    Fix Deletions

--- projectilecalc.py
+++ projectilecalc.py
@@ -1,8 +1,11 @@
+__acrimony__ = 'dev'
+__jluo0117__ ='dev2'
+
+import math
+
 print ("Welcome to Projectile Calculator")
 angle = input("Enter the angle of launch in degrees: ")
 velocity = input("Enter the velocity at launch in meters per second: ")
-gravity = input("Enter the acceleration of gravity as a negative number: ")
-
 print ("Earth = - 9.81")
 print ("Mars = - 6.301")
 print ("Moon = - 1.625")
@@ -12,8 +15,13 @@ if gravity >0:
 radian_angle = math.radians(angle) #converts the angle in degrees to radians
 y_velocity = math.sin(radian_angle) # velocity #gets y-component of velocity
 
-time_top = (-y_velocity)/gravity # calculates the time to the top of the trajectory 
 time_top = (-y_velocity)/gravity # calculates the time to the top of the trajectory
 
 total_time = time_top*2 #conservation of energy, duh.
 
+x_velocity = math.cos(radian_angle) * velocity # finds the x-component of velocity
+
+distance = x_velocity * total_time # total distance traveled, 'nough said
+
+print ("Total time traveled: {}".format(total_time))
+print ("Total distance traveled: {}".format(distance))
