dad15e0708621eaaf294875cb70bda5c1ab8e1a0
James Luo <luojames52@gmail.com>
2016-11-21 21:55:08 -0800

    Added Known Gravity Values (#1)
    * Added Known Gravity Values
    
    Added values for Mars, Earth, and Moon along with not needing to enter a negative value for gravity.
    
    * Fix Deletions
    
    * Line Added Above Gravity Values
    
    I added a line above the gravity values to make it clear to the use what the meaning of those numbers was.

--- projectilecalc.py
+++ projectilecalc.py
@@ -1,15 +1,22 @@
 __acrimony__ = 'dev'
+__jluo0117__ ='dev2'
+
 import math
 
 print ("Welcome to Projectile Calculator")
 angle = input("Enter the angle of launch in degrees: ")
 velocity = input("Enter the velocity at launch in meters per second: ")
-gravity = input("Enter the acceleration of gravity as a negative number: ")
-
+print("Acceleration of gravity on different planets/satellites: ")
+print ("Earth = - 9.81")
+print ("Mars = - 6.301")
+print ("Moon = - 1.625")
+gravity = input("Enter the acceleration of gravity ")
+if gravity >0:
+    gravity=gravity*-1
 radian_angle = math.radians(angle) #converts the angle in degrees to radians
 y_velocity = math.sin(radian_angle) # velocity #gets y-component of velocity
 
-time_top = (-y_velocity)/gravity # calculates the time to the top of the trajectory 
+time_top = (-y_velocity)/gravity # calculates the time to the top of the trajectory
 
 total_time = time_top*2 #conservation of energy, duh.
 
