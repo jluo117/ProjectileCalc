ff40a621b10c8a0bf5c27bdee1b4c8577ec8cab3
James Luo <luojames52@gmail.com>
2016-11-23 12:07:23 -0800

    clarify velocity equation and energy of system
    Ask user to enter velocity in terms of getting closer to origin or farther away. 
    along with the calculation of the total energy in the system

--- inellastic-collision.py
+++ inellastic-collision.py
@@ -5,12 +5,13 @@ import math
 #m1v1+m2v2= (m1+m2)vf
 
 print ("Welcome to Inellastic Calc")
-	
+print ("If object is going to the right then enter positive. Else enter a negative number.")
 mass1 = input("Enter the mass of object 1: ")
-v1 = input("Enter the velocity value  for object 1: ")
+v1 = input("Enter the velocity value for object 1: ")
 mass2 = input("Enter the mass of object 2: ")
 v2 =  input("Enter the velocity value of object 2: ")	
 vf = ((mass1*v1)+(mass2*v2))/(mass1+mass2)
-
+energy = (mass1*v1)+(mass*v2)
+print ("The energy in the system is :{} N".format(energy))
 print("Your final velocity is:{}m/s".format(vf))
  	
