b929cc2079cc7aa8b448c312304ac2aae543273b
James Luo <luojames52@gmail.com>
2016-11-23 14:20:46 -0800

    Update inellastic-collision.py

--- inellastic-collision.py
+++ inellastic-collision.py
@@ -11,7 +11,6 @@ v1 = input("Enter the velocity value for object 1: ")
 mass2 = input("Enter the mass of object 2: ")
 v2 =  input("Enter the velocity value of object 2: ")	
 vf = ((mass1*v1)+(mass2*v2))/(mass1+mass2)
-energy = (mass1*v1)+(mass*v2)
 print ("The energy in the system is :{} N".format(energy))
 print("Your final velocity is:{}m/s".format(vf))
  	
